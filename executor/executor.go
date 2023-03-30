272d2e6d2398f2f2cc37a819003aa1519385a01b
zhenorzz <zhenorzz@gmail.com>
2023-03-30 17:05:49 +0800

    Optimize channel

--- executor/executor.go
+++ executor/executor.go
@@ -7,8 +7,13 @@ import (
 	"github.com/gotodb/gotodb/logger"
 	"github.com/gotodb/gotodb/pb"
 	"github.com/gotodb/gotodb/stage"
+	"github.com/gotodb/gotodb/util"
 	"github.com/vmihailenco/msgpack"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"io"
+	"net"
+	"strings"
 	"sync"
 )
 
@@ -66,7 +71,7 @@ func (e *Executor) AddLogInfo(info interface{}, level pb.LogLevel) {
 
 func (e *Executor) Clear() {
 	for _, writer := range e.Writers {
-		writer.(io.WriteCloser).Close()
+		_ = writer.(io.WriteCloser).Close()
 	}
 	e.IsStatusChanged = true
 	if e.Status != pb.TaskStatus_ERROR {
@@ -138,6 +143,107 @@ func (e *Executor) SendInstruction(ctx context.Context, instruction *pb.Instruct
 		e.Status = pb.TaskStatus_TODO
 		return res, fmt.Errorf("unknown node type")
 	}
+}
+
+func (e *Executor) SetupWriters(ctx context.Context, empty *pb.Empty) (*pb.Empty, error) {
+	logger.Infof("SetupWriters start")
+	var err error
+
+	ip := strings.Split(e.Instruction.Location.Address, ":")[0]
+
+	for range e.StageJob.GetOutputs() {
+		pr, pw := io.Pipe()
+		e.Writers = append(e.Writers, pw)
+		listener, err := net.Listen("tcp", ip+":0")
+		if err != nil {
+			logger.Errorf("failed to open listener: %v", err)
+			return nil, fmt.Errorf("failed to open listener: %v", err)
+		}
+		e.OutputChannelLocations = append(e.OutputChannelLocations,
+			&pb.Location{
+				Name:    e.Name,
+				Address: util.GetHostFromAddress(listener.Addr().String()),
+				Port:    util.GetPortFromAddress(listener.Addr().String()),
+			},
+		)
+
+		go func() {
+			for {
+				select {
+				case <-e.DoneChan:
+					_ = listener.Close()
+					return
+
+				default:
+					conn, err := listener.Accept()
+					if err != nil {
+						logger.Errorf("failed to accept: %v", err)
+						continue
+					}
+					logger.Infof("connect %v", conn)
+
+					go func(w io.Writer) {
+						err := util.CopyBuffer(pr, w)
+						if err != nil && err != io.EOF {
+							logger.Errorf("failed to CopyBuffer: %v", err)
+						}
+						if wc, ok := w.(io.WriteCloser); ok {
+							_ = wc.Close()
+						}
+					}(conn)
+				}
+			}
+		}()
+	}
+
+	logger.Infof("SetupWriters Input=%v, Output=%v", e.InputChannelLocations, e.OutputChannelLocations)
+	return empty, err
+}
+
+func (e *Executor) SetupReaders(ctx context.Context, empty *pb.Empty) (*pb.Empty, error) {
+	var err error
+	logger.Infof("SetupReaders start")
+	for _, location := range e.StageJob.GetInputs() {
+		pr, pw := io.Pipe()
+		e.Readers = append(e.Readers, pr)
+
+		conn, err := grpc.Dial(location.GetURL(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			logger.Errorf("failed to connect to %v: %v", location, err)
+			return empty, err
+		}
+		client := pb.NewWorkerClient(conn)
+		inputChannelLocation, err := client.GetOutputChannelLocation(context.Background(), location)
+
+		if err != nil {
+			logger.Errorf("failed to connect %v: %v", location, err)
+			return empty, err
+		}
+
+		_ = conn.Close()
+
+		e.InputChannelLocations = append(e.InputChannelLocations, inputChannelLocation)
+		cconn, err := net.Dial("tcp", inputChannelLocation.GetURL())
+		if err != nil {
+			logger.Errorf("failed to connect to input channel %v: %v", inputChannelLocation, err)
+			return empty, err
+		}
+		logger.Infof("connect to %v", inputChannelLocation)
+
+		go func(r io.Reader) {
+			err := util.CopyBuffer(r, pw)
+			if err != nil && err != io.EOF {
+				logger.Errorf("failed to CopyBuffer: %v", err)
+			}
+			_ = pw.Close()
+			if rc, ok := r.(io.ReadCloser); ok {
+				_ = rc.Close()
+			}
+		}(cconn)
+	}
+
+	logger.Infof("SetupReaders Input=%v, Output=%v", e.InputChannelLocations, e.OutputChannelLocations)
+	return empty, err
 }
 
 func (e *Executor) Run(ctx context.Context, empty *pb.Empty) (*pb.Empty, error) {
